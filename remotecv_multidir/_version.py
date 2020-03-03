092b6febadfa70d9a1af5d4406ba05f066d9e600
benneic <benn@eichhorn.co>
2020-03-03 12:11:34 +1100

    load from env var and parse as json

--- remotecv_multidir/_version.py
+++ remotecv_multidir/_version.py
@@ -2,4 +2,4 @@
 This module defines the package version for use in __init__.py and
 setup.py.
 """
-__version__ = '0.1.0'
+__version__ = '0.1.1'
--- remotecv_multidir/loader.py
+++ remotecv_multidir/loader.py
@@ -1,11 +1,13 @@
 # coding: utf-8
 
 from remotecv.utils import config
-from . import multidir_config
 
 import urllib2
 import re
 import os
+import json
+from os.path import join, exists, abspath
+from six.moves.urllib.parse import unquote
 
 import logging
 
@@ -16,12 +18,16 @@ if not isinstance(loglevel, int):
 logger.setLevel(loglevel)
 
 
+# The list of paths where the File Loader will try to find images
+REMOTECV_MULTIDIR_LOADER_DIRS = json.loads(os.environ.get('REMOTECV_MULTIDIR_LOADER_DIRS', '[]'))
+
+
 def load_sync(path):
     """
     Loads image from a list of directories
     :param string path: Path to load
     """
-    for idx, next_dir in enumerate(multidir_config.REMOTECV_MULTIDIR_LOADER_DIRS):
+    for idx, next_dir in enumerate(REMOTECV_MULTIDIR_LOADER_DIRS):
 
         file_path = join(next_dir.rstrip('/'), path.lstrip('/'))
         file_path = abspath(file_path)
@@ -47,7 +53,7 @@ def load_sync(path):
         logger.debug('File {0} not found in {1}'.format(path, next_dir))
         # else loop and try next directory
     
-    if not multidir_config.REMOTECV_MULTIDIR_LOADER_DIRS:
+    if not REMOTECV_MULTIDIR_LOADER_DIRS:
         logger.error('No paths set in environment var REMOTECV_MULTIDIR_LOADER_DIRS')
 
     # no file found
